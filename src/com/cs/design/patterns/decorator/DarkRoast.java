f73d761b035804a1f1f4acd53c884f20db12e4b2
parashuram-patil <parasharam_patil@ymail.com>
2019-01-08 15:49:37 +0530

    Decorator Design Pattern - used to modify the functionality of an object at runtime - Encpsulate same type as class property, and nest same method of it - used a lot in Java IO classes, such as FileReader, BufferedReader etc.

---
+++ src/com/cs/design/patterns/decorator/DarkRoast.java
@@ -0,0 +1,12 @@
+package com.cs.design.patterns.decorator;
+
+public class DarkRoast extends Beverage {
+	public DarkRoast() {
+		description = "Dark Roast Coffee";
+	}
+ 
+	public double cost() {
+		return .99;
+	}
+}
+
